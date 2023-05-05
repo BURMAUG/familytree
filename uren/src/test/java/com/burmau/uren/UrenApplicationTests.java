68855ee7e4e2a191a6b8f0ec0b9e71f47a1a1d6d
Burmau Garba <burmaud1@gmail.com>
2023-05-05 17:10:00 -0400

    initial commit

---
+++ uren/src/test/java/com/burmau/uren/UrenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burmau.uren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
